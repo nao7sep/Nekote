907d7afb1eaceb3ce7c7e0d25aaac323d0d7b8a6
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-10-19 12:44:39 +0900

    Add roundtrip tests for DateTimeHelper and TimeSpanHelper; enhance random value generation methods

--- tests/Nekote.Core.Tests/Time/DateTimeHelperTests.cs
+++ tests/Nekote.Core.Tests/Time/DateTimeHelperTests.cs
@@ -1,3 +1,5 @@
+using System;
+using Nekote.Core.Time;
 using Xunit;
 
 namespace Nekote.Core.Tests.Time
@@ -7,5 +9,187 @@ namespace Nekote.Core.Tests.Time
     /// </summary>
     public class DateTimeHelperTests
     {
+        private static readonly Random _random = new();
+
+        /// <summary>
+        /// 指定された種類のランダムな <see cref="DateTimeOffset"/> を作成します。
+        /// </summary>
+        private static DateTimeOffset CreateRandomDateTimeOffset(DateTimeKind kind)
+        {
+            var ticks = (long)(_random.NextDouble() * DateTime.MaxValue.Ticks);
+            var dateTime = new DateTime(ticks, kind == DateTimeKind.Unspecified ? DateTimeKind.Local : kind);
+            if (kind == DateTimeKind.Utc)
+            {
+                return new DateTimeOffset(dateTime.ToUniversalTime());
+            }
+            return new DateTimeOffset(dateTime.ToLocalTime());
+        }
+
+        /// <summary>
+        /// 指定された種類のランダムな <see cref="DateTime"/> を作成します。
+        /// </summary>
+        private static DateTime CreateRandomDateTime(DateTimeKind kind)
+        {
+            var ticks = (long)(_random.NextDouble() * DateTime.MaxValue.Ticks);
+            return new DateTime(ticks, kind);
+        }
+
+        /// <summary>
+        /// ランダムな <see cref="DateOnly"/> を作成します。
+        /// </summary>
+        private static DateOnly CreateRandomDateOnly()
+        {
+            var year = _random.Next(1, 10000);
+            var month = _random.Next(1, 13);
+            var day = _random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+            return new DateOnly(year, month, day);
+        }
+
+        /// <summary>
+        /// ランダムな <see cref="TimeOnly"/> を作成します。
+        /// </summary>
+        private static TimeOnly CreateRandomTimeOnly()
+        {
+            var ticks = (long)(_random.NextDouble() * TimeOnly.MaxValue.Ticks);
+            return new TimeOnly(ticks);
+        }
+
+        /// <summary>
+        /// 書式指定子から <see cref="DateTimeKind"/> を取得します。
+        /// </summary>
+        private static DateTimeKind GetDateTimeKindFromFormat(DateTimeFormatKind format)
+        {
+            var name = format.ToString();
+            if (name.StartsWith("Utc")) return DateTimeKind.Utc;
+            if (name.StartsWith("Local")) return DateTimeKind.Local;
+            return DateTimeKind.Unspecified;
+        }
+
+        /// <summary>
+        /// <see cref="DateTimeOffset"/> のラウンドトリップテストです。
+        /// 注意: DateTimeFormatKind のうち、日付のみ・時刻のみの書式（DateSortable, DateUserFriendly, TimeSortable など）は
+        /// DateTimeOffset には適用できないため、ここではテストしていません。
+        /// これらの書式は DateTime_Roundtrip, DateOnly_Roundtrip, TimeOnly_Roundtrip でカバーされています。
+        /// </summary>
+        [Theory]
+        [InlineData(DateTimeFormatKind.LocalSortable)]
+        [InlineData(DateTimeFormatKind.LocalSortableMilliseconds)]
+        [InlineData(DateTimeFormatKind.LocalSortableTicks)]
+        [InlineData(DateTimeFormatKind.UtcSortable)]
+        [InlineData(DateTimeFormatKind.UtcSortableMilliseconds)]
+        [InlineData(DateTimeFormatKind.UtcSortableTicks)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlyMinutes)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlySeconds)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlyMilliseconds)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlyTicks)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlyMinutes)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlySeconds)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlyMilliseconds)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlyTicks)]
+        public void DateTimeOffset_Roundtrip(DateTimeFormatKind format)
+        {
+            var dateTimeKind = GetDateTimeKindFromFormat(format);
+            var originalValue = CreateRandomDateTimeOffset(dateTimeKind);
+            var sourceString = originalValue.ToString(format);
+            var formatString = DateTimeFormats.GetFormatString(format);
+            var expectedString = originalValue.ToString(formatString);
+
+            var parsedValue = DateTimeHelper.ParseDateTimeOffset(sourceString, format);
+            var actualParsedString = parsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualParsedString);
+
+            Assert.True(DateTimeHelper.TryParseDateTimeOffset(sourceString, format, out var tryParsedValue));
+            var actualTryParsedString = tryParsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualTryParsedString);
+        }
+
+        /// <summary>
+        /// <see cref="DateTime"/> のラウンドトリップテストです。
+        /// </summary>
+        [Theory]
+        [InlineData(DateTimeFormatKind.LocalSortable)]
+        [InlineData(DateTimeFormatKind.LocalSortableMilliseconds)]
+        [InlineData(DateTimeFormatKind.LocalSortableTicks)]
+        [InlineData(DateTimeFormatKind.UtcSortable)]
+        [InlineData(DateTimeFormatKind.UtcSortableMilliseconds)]
+        [InlineData(DateTimeFormatKind.UtcSortableTicks)]
+        [InlineData(DateTimeFormatKind.DateSortable)]
+        [InlineData(DateTimeFormatKind.TimeSortable)]
+        [InlineData(DateTimeFormatKind.TimeSortableMilliseconds)]
+        [InlineData(DateTimeFormatKind.TimeSortableTicks)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlyMinutes)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlySeconds)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlyMilliseconds)]
+        [InlineData(DateTimeFormatKind.LocalUserFriendlyTicks)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlyMinutes)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlySeconds)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlyMilliseconds)]
+        [InlineData(DateTimeFormatKind.UtcUserFriendlyTicks)]
+        [InlineData(DateTimeFormatKind.DateUserFriendly)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlyMinutes)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlySeconds)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlyMilliseconds)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlyTicks)]
+        public void DateTime_Roundtrip(DateTimeFormatKind format)
+        {
+            var dateTimeKind = GetDateTimeKindFromFormat(format);
+            var originalValue = CreateRandomDateTime(dateTimeKind);
+            var sourceString = originalValue.ToString(format);
+            var formatString = DateTimeFormats.GetFormatString(format);
+            var expectedString = originalValue.ToString(formatString);
+
+            var parsedValue = DateTimeHelper.ParseDateTime(sourceString, format);
+            var actualParsedString = parsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualParsedString);
+
+            Assert.True(DateTimeHelper.TryParseDateTime(sourceString, format, out var tryParsedValue));
+            var actualTryParsedString = tryParsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualTryParsedString);
+        }
+
+        /// <summary>
+        /// <see cref="DateOnly"/> のラウンドトリップテストです。
+        /// </summary>
+        [Theory]
+        [InlineData(DateTimeFormatKind.DateSortable)]
+        [InlineData(DateTimeFormatKind.DateUserFriendly)]
+        public void DateOnly_Roundtrip(DateTimeFormatKind format)
+        {
+            var expectedValue = CreateRandomDateOnly();
+            var sourceString = expectedValue.ToString(format);
+
+            var parsedValue = DateTimeHelper.ParseDateOnly(sourceString, format);
+            Assert.Equal(expectedValue, parsedValue);
+
+            Assert.True(DateTimeHelper.TryParseDateOnly(sourceString, format, out var tryParsedValue));
+            Assert.Equal(expectedValue, tryParsedValue);
+        }
+
+        /// <summary>
+        /// <see cref="TimeOnly"/> のラウンドトリップテストです。
+        /// </summary>
+        [Theory]
+        [InlineData(DateTimeFormatKind.TimeSortable)]
+        [InlineData(DateTimeFormatKind.TimeSortableMilliseconds)]
+        [InlineData(DateTimeFormatKind.TimeSortableTicks)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlyMinutes)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlySeconds)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlyMilliseconds)]
+        [InlineData(DateTimeFormatKind.TimeUserFriendlyTicks)]
+        public void TimeOnly_Roundtrip(DateTimeFormatKind format)
+        {
+            var originalValue = CreateRandomTimeOnly();
+            var sourceString = originalValue.ToString(format);
+            var formatString = DateTimeFormats.GetFormatString(format);
+            var expectedString = originalValue.ToString(formatString);
+
+            var parsedValue = DateTimeHelper.ParseTimeOnly(sourceString, format);
+            var actualParsedString = parsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualParsedString);
+
+            Assert.True(DateTimeHelper.TryParseTimeOnly(sourceString, format, out var tryParsedValue));
+            var actualTryParsedString = tryParsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualTryParsedString);
+        }
     }
 }
--- tests/Nekote.Core.Tests/Time/TimeSpanHelperTests.cs
+++ tests/Nekote.Core.Tests/Time/TimeSpanHelperTests.cs
@@ -1,3 +1,5 @@
+using System;
+using Nekote.Core.Time;
 using Xunit;
 
 namespace Nekote.Core.Tests.Time
@@ -7,5 +9,41 @@ namespace Nekote.Core.Tests.Time
     /// </summary>
     public class TimeSpanHelperTests
     {
+        private static readonly Random _random = new();
+
+        /// <summary>
+        /// ランダムな <see cref="TimeSpan"/> を作成します。
+        /// </summary>
+        private static TimeSpan CreateRandomTimeSpan()
+        {
+            return new TimeSpan(_random.Next(0, 24), _random.Next(0, 60), _random.Next(0, 60));
+        }
+
+        /// <summary>
+        /// <see cref="TimeSpan"/> のラウンドトリップテストです。
+        /// </summary>
+        [Theory]
+        [InlineData(TimeSpanFormatKind.SortableSeconds)]
+        [InlineData(TimeSpanFormatKind.SortableMilliseconds)]
+        [InlineData(TimeSpanFormatKind.SortableTicks)]
+        [InlineData(TimeSpanFormatKind.UserFriendlyMinutes)]
+        [InlineData(TimeSpanFormatKind.UserFriendlySeconds)]
+        [InlineData(TimeSpanFormatKind.UserFriendlyMilliseconds)]
+        [InlineData(TimeSpanFormatKind.UserFriendlyTicks)]
+        public void TimeSpan_Roundtrip(TimeSpanFormatKind format)
+        {
+            var originalValue = CreateRandomTimeSpan();
+            var formattedString = originalValue.ToString(format);
+            var formatString = TimeSpanFormats.GetFormatString(format);
+            var expectedString = originalValue.ToString(formatString);
+
+            var parsedValue = TimeSpanHelper.ParseTimeSpan(formattedString, format);
+            var actualParsedString = parsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualParsedString);
+
+            Assert.True(TimeSpanHelper.TryParseTimeSpan(formattedString, format, out var tryParsedValue));
+            var actualTryParsedString = tryParsedValue.ToString(formatString);
+            Assert.Equal(expectedString, actualTryParsedString);
+        }
     }
 }

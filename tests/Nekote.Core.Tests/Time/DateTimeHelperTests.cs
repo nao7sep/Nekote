fb532a82b78dd5d8f56dbd4fd3e974a78ee6112a
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-10-19 11:21:06 +0900

    Add test project and initial test classes for DateTimeHelper and TimeSpanHelper

---
+++ tests/Nekote.Core.Tests/Time/DateTimeHelperTests.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Nekote.Core.Tests.Time
+{
+    /// <summary>
+    /// <see cref="DateTimeHelper"/> のテストクラスです。
+    /// </summary>
+    public class DateTimeHelperTests
+    {
+    }
+}
